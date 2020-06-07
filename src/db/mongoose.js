126881dd0e94ed372e9c6845d932b8c55e090323
Kevin Cabrera <kfvf671@astrazeneca.net>
2020-06-07 17:24:14 -0500

    use validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 
@@ -8,47 +9,65 @@ mongoose.connect(connectionURL, {
     useUnifiedTopology: true
 });
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-});
-
-const task = new Task({
-    description: 'Learn Mocha',
-    completed: 'fell'
-});
-
-task.save().then(
-    () => {
-        console.log(task);
-    }
-).catch((error) => {
-    console.log('Error! ', error);
-});
-
-
-// const User = mongoose.model('User', {
-//     name: {
+// const Task = mongoose.model('Task', {
+//     description: {
 //         type: String
 //     },
-//     age: {
-//         type: Number
+//     completed: {
+//         type: Boolean
 //     }
 // });
 
-// const me = new User({
-//     name: 'Kevin',
-//     age: 24
+// const task = new Task({
+//     description: 'Learn Mocha',
+//     completed: 'fell'
 // });
 
-// me.save().then(
+// task.save().then(
 //     () => {
-//         console.log(me);
+//         console.log(task);
 //     }
 // ).catch((error) => {
 //     console.log('Error! ', error);
 // });
+
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    }
+});
+
+const me = new User({
+    name: 'Teddy',
+    email: 'teddy@bear.com'
+});
+
+me.save().then(
+    () => {
+        console.log(me);
+    }
+).catch((error) => {
+    console.log('Error! ', error);
+});
