4fb9529de506916bab98c6a7a06026e4455cb1ca
Kevin Cabrera <kfvf671@astrazeneca.net>
2020-06-07 17:52:20 -0500

    Challenge 86: Valitdations in moongose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -9,65 +9,79 @@ mongoose.connect(connectionURL, {
     useUnifiedTopology: true
 });
 
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
-// });
-
-// const task = new Task({
-//     description: 'Learn Mocha',
-//     completed: 'fell'
-// });
-
-// task.save().then(
-//     () => {
-//         console.log(task);
-//     }
-// ).catch((error) => {
-//     console.log('Error! ', error);
-// });
-
-
-const User = mongoose.model('User', {
-    name: {
+const Task = mongoose.model('Task', {
+    description: {
         type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
         trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
     }
 });
 
-const me = new User({
-    name: 'Teddy',
-    email: 'teddy@bear.com'
+const task = new Task({
+    description: 'Learn Node JS',
 });
 
-me.save().then(
+task.save().then(
     () => {
-        console.log(me);
+        console.log(task);
     }
 ).catch((error) => {
     console.log('Error! ', error);
 });
+
+
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     age: {
+//         type: Number,
+//         validate(value) {
+//             if (value < 0) {
+//                 throw new Error('Age must be a positive number');
+//             }
+//         }
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('Email is invalid')
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 7,
+//         validate(value) {
+//             if (value.toLowerCase().includes('password')) {
+//                 throw new Error('You can\'t set password to password')
+//             }
+//         }
+//     }
+// });
+
+// const me = new User({
+//     name: 'John Lennon',
+//     email: 'thebeatles@band.com',
+//     password: '12345 '
+// });
+
+// me.save().then(
+//     () => {
+//         console.log(me);
+//     }
+// ).catch((error) => {
+//     console.log('Error! ', error);
+// });
