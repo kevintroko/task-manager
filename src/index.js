d5068b0c49434a35bf1df8f692cd286d288329cf
Kevin Cabrera <kfvf671@astrazeneca.net>
2020-06-08 16:47:16 -0500

    Challenge 100: Delete

--- src/index.js
+++ src/index.js
@@ -70,6 +70,18 @@ app.patch('/users/:id', async (req, res) => {
     }
 });
 
+app.delete('/users/:id', async(req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send(error);
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
     try {
@@ -124,6 +136,18 @@ app.get('/tasks', async (req, res) => {
         res.send(tasks);
     } catch (error) {
         res.status(500).send(error);
+    }
+});
+
+app.delete('/tasks/:id', async(req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) {
+            return res.status(404).send(error);
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(400).send(error);
     }
 });
 
