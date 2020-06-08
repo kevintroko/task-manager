dad2a3b9be599b25d9d6cae16fe80fd2cab97744
Kevin Cabrera <kfvf671@astrazeneca.net>
2020-06-08 14:45:44 -0500

    Refactor chained promises with asyncc/await

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -3,15 +3,26 @@ const User = require('../src/models/user');
 
 // 5ede499201be226111ea6ef6
 
-User.findByIdAndUpdate('5ede4dd97ce03a645e3a8cc5', { age: 1 }).then(
-    (user) => {
-        console.log(user);
-        return User.countDocuments({
-            age: 1
-        });
-    }
-).then((result) => {
-    console.log(result);
-}).catch((e) => {
-    console.log(e);
-});
+// User.findByIdAndUpdate('5ede4dd97ce03a645e3a8cc5', { age: 1 }).then(
+//     (user) => {
+//         console.log(user);
+//         return User.countDocuments({
+//             age: 1
+//         });
+//     }
+// ).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+const updateAgeAndCount = async(id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({age});
+    return count;
+};
+
+updateAgeAndCount('5ede4dd97ce03a645e3a8cc5', 24)
+    .then((count) => console.log(count))
+    .catch(e => console.log(e)
+);
