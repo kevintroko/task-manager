e16bed6ac16634dbd354b490faa1c076cf70ffcd
Kevin Cabrera <kfvf671@astrazeneca.net>
2020-06-08 15:10:10 -0500

    Refactor index.js to be asyc/await

--- src/index.js
+++ src/index.js
@@ -8,38 +8,39 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    user.save().then(() => {
-        res.send(user);
-    }).catch((e) => {
-        res.status(400).send(e);
-    });
+
+    try {
+        await user.save();
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) =>
-        res.send(users)
-    ).catch((e) =>
-        res.status(500).send(e)
-    )
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(500).send(error)
+    }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async(req, res) => {
     const _id = req.params.id;
     console.log(_id);
 
-    User.findById(_id).then((user) => {
-        console.log(user);
-
+    try {
+        const user = await User.findById(_id);
         if (!user) {
             return res.status(404).send()
         }
-        res.send(user)
-    }).catch((e) => {
-        console.log(e);
-        res.status(500).send(e)
-    });
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error)
+    }
 });
 
 app.post('/tasks', (req, res) => {
