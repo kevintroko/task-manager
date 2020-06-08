dede099e169a2375a053559301ff0bfbb7e76433
Kevin Cabrera <kfvf671@astrazeneca.net>
2020-06-08 09:46:41 -0500

    GET and POSTS

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 
@@ -8,80 +7,3 @@ mongoose.connect(connectionURL, {
     useCreateIndex: true,
     useUnifiedTopology: true
 });
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
-
-const task = new Task({
-    description: 'Learn Node JS',
-});
-
-task.save().then(
-    () => {
-        console.log(task);
-    }
-).catch((error) => {
-    console.log('Error! ', error);
-});
-
-
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     age: {
-//         type: Number,
-//         validate(value) {
-//             if (value < 0) {
-//                 throw new Error('Age must be a positive number');
-//             }
-//         }
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         lowercase: true,
-//         validate(value) {
-//             if (!validator.isEmail(value)) {
-//                 throw new Error('Email is invalid')
-//             }
-//         }
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         minlength: 7,
-//         validate(value) {
-//             if (value.toLowerCase().includes('password')) {
-//                 throw new Error('You can\'t set password to password')
-//             }
-//         }
-//     }
-// });
-
-// const me = new User({
-//     name: 'John Lennon',
-//     email: 'thebeatles@band.com',
-//     password: '12345 '
-// });
-
-// me.save().then(
-//     () => {
-//         console.log(me);
-//     }
-// ).catch((error) => {
-//     console.log('Error! ', error);
-// });
